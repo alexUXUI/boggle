69df5383be049e4e9f4353b0624c46fba1f5c6c1
Alex Bennett <alex.bennett@productscience.ai>
2023-01-26 10:24:16 -0700

    abstract animations

---
+++ src/routes/boggle/animations.ts
@@ -0,0 +1,74 @@
+import { $ } from '@builder.io/qwik';
+export const confetti = import('canvas-confetti');
+
+export const fireWorks = $(() => {
+  confetti.then((module) => {
+    const count = 200;
+    const defaults = {};
+
+    function fire(particleRatio: number, opts: any) {
+      module.default(
+        Object.assign({}, defaults, opts, {
+          particleCount: Math.floor(count * particleRatio) * 2,
+          colors: ['#0000af', '#05b5eb', '#0051ba', '#230ee2', '#1f3bc6'],
+        })
+      );
+    }
+
+    fire(0.25, {
+      spread: 46,
+      startVelocity: 55,
+      origin: { x: 0.5, y: 1 },
+      decay: 0.87,
+      scalar: 1.2,
+    });
+  });
+});
+
+export const confettiCelebration = $(() => {
+  setTimeout(() => {
+    const duration = 15 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = {
+      startVelocity: 30,
+      spread: 360,
+      ticks: 60,
+      zIndex: 0,
+    };
+
+    confetti.then((module) => {
+      function randomInRange(min: number, max: number) {
+        return Math.random() * (max - min) + min;
+      }
+
+      const interval: ReturnType<typeof setTimeout> = setInterval(function () {
+        const timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+          return clearInterval(interval);
+        }
+
+        const particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        module.default(
+          Object.assign({}, defaults, {
+            particleCount,
+            origin: {
+              x: randomInRange(0.1, 0.3),
+              y: Math.random() - 0.2,
+            },
+          })
+        );
+        module.default(
+          Object.assign({}, defaults, {
+            particleCount,
+            origin: {
+              x: randomInRange(0.7, 0.9),
+              y: Math.random() - 0.2,
+            },
+          })
+        );
+      }, 250);
+    });
+  }, 100);
+});
--- src/routes/boggle/index.tsx
+++ src/routes/boggle/index.tsx
@@ -6,21 +6,14 @@ import {
 } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import { Answers } from './components/Answers';
-import {
-  generateRandomBoard,
-  generateRandomRussianBoard,
-} from './logic/generateBoard';
+import { generateRandomRussianBoard } from './logic/generateBoard';
 import { BoggleGrid } from './components/BoggleGrid';
 import { Controls } from './components/Controls';
 import { FoundWords } from './components/FoundWords';
-import {
-  LANGUAGE,
-  Language,
-  getDictionary,
-  importWordsFromPublicDir,
-} from './logic/api';
+import type { Language } from './logic/api';
+import { LANGUAGE, getDictionary } from './logic/api';
 import { solve } from './logic/boggle';
-export const confetti = import('canvas-confetti');
+import { confettiCelebration, fireWorks } from './animations';
 
 export interface State {
   boardSize: number;
@@ -58,14 +51,6 @@ export default component$(() => {
   // when the component mounts, fetch the dictionary
   useClientEffect$(() => {
     if (!dictionary.data.length) {
-      // importWordsFromPublicDir().then((data) => {
-      //   dictionary.data = data;
-      //   answers.data = solve(data, state.board).filter((value: string) => {
-      //     return value.length >= state.minWordLength;
-      //   });
-      //   state.isLoaded = true;
-      // });
-
       getDictionary(LANGUAGE.RUSSIAN).then((data) => {
         dictionary.data = data;
         answers.data = solve(data, state.board).filter((value: string) => {
@@ -144,38 +129,15 @@ export default component$(() => {
           foundWords.data = [...foundWords.data, word];
           state.selectedPath = [];
           state.wordFound = false;
-          confetti.then((module) => {
-            const count = 200;
-            const defaults = {};
-
-            function fire(particleRatio: number, opts: any) {
-              module.default(
-                Object.assign({}, defaults, opts, {
-                  particleCount: Math.floor(count * particleRatio) * 2,
-                  colors: [
-                    '#0000af',
-                    '#05b5eb',
-                    '#0051ba',
-                    '#230ee2',
-                    '#1f3bc6',
-                  ],
-                })
-              );
-            }
-
-            fire(0.25, {
-              spread: 46,
-              startVelocity: 55,
-              origin: { x: 0.5, y: 1 },
-              decay: 0.87,
-              scalar: 1.2,
-            });
-          });
+          fireWorks();
         }, 100);
       }
     }
   });
 
+  /**
+   * When the user finds all the words, show fireworks
+   */
   useTask$(({ track }) => {
     track(() => foundWords.data);
     track(() => answers.data);
@@ -183,57 +145,8 @@ export default component$(() => {
     const answersLength = answers.data.length;
     const foundWordsLength = foundWords.data.length - 1;
 
-    console.log(foundWordsLength, answersLength);
-
     if (foundWordsLength && foundWordsLength === answersLength) {
-      setTimeout(() => {
-        const duration = 15 * 1000;
-        const animationEnd = Date.now() + duration;
-        const defaults = {
-          startVelocity: 30,
-          spread: 360,
-          ticks: 60,
-          zIndex: 0,
-        };
-
-        confetti.then((module) => {
-          function randomInRange(min: number, max: number) {
-            return Math.random() * (max - min) + min;
-          }
-
-          const interval: ReturnType<typeof setTimeout> = setInterval(
-            function () {
-              const timeLeft = animationEnd - Date.now();
-
-              if (timeLeft <= 0) {
-                return clearInterval(interval);
-              }
-
-              const particleCount = 50 * (timeLeft / duration);
-              // since particles fall down, start a bit higher than random
-              module.default(
-                Object.assign({}, defaults, {
-                  particleCount,
-                  origin: {
-                    x: randomInRange(0.1, 0.3),
-                    y: Math.random() - 0.2,
-                  },
-                })
-              );
-              module.default(
-                Object.assign({}, defaults, {
-                  particleCount,
-                  origin: {
-                    x: randomInRange(0.7, 0.9),
-                    y: Math.random() - 0.2,
-                  },
-                })
-              );
-            },
-            250
-          );
-        });
-      }, 100);
+      confettiCelebration();
     }
   });
 
